ffc69ab7eea48f0dc9ab05a844ee2e784661f643
Dave North <dave.north@rewind.io>
2019-02-28 08:48:52 -0500

    Fix SQL

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-echo "Running tests 4"
+RESULT=0
 
-PGPASSWORD=password psql -h postgres -U postgres -d backup_test -c "select count(*) from schema_migrations"
+echo "Running tests"
 
-exit 0
+export PGPASSWORD=password 
+
+COUNT=$(psql -t -h postgres -U postgres -d backup_test -c "select count(*) from schema_migrations" | xargs)
+
+echo "the count is ${COUNT}"
+
+if [ "${COUNT}" -ne 4 ]; then
+    echo "Tests failed"
+    RESULT=1
+else
+    echo "Tests passed"
+    RESULT=0
+fi 
+
+exit ${RESULT}
